a3c4a2dcb8e46776617c8ddb32cf623d966dfba1
hornsbeef <login.horninger@gmail.com>
2024-03-30 14:09:57 +0100

    task3a.c updated

--- exercises/exercise03/task3/task3a.c
+++ exercises/exercise03/task3/task3a.c
@@ -8,6 +8,30 @@
 //Write a program that initializes a global variable accumulation of type int64_t and initializes it to 0.
 int64_t accumulation = 0;
 
+int64_t accumulate(int64_t global_acc) {
+    //The child accumulates the values from 1 to N = 10 into accumulation and exits.
+    for(int i = 1; i <= 10; i++ ){
+        global_acc += i;
+    }
+    return global_acc;
+}
+
+void* pthreadStartRoutine(void* void_ptr){
+    //The thread does the same thing as the child process, i.e. accumulate the values from 1to N = 10 into the global variable and immediately exit.
+    accumulation = accumulate(accumulation);
+    pthread_exit(NULL);
+    return NULL;
+}
+
+void pthread_error_funct(int pthread_returnValue) {
+    if(pthread_returnValue != 0){
+        fprintf(stderr, "pthread creation failed! Error code: %d"
+                        "\nNote that the pthreads functions do not set errno.",
+                        pthread_returnValue);
+        exit(EXIT_FAILURE);
+    }
+}
+
 int main(){
 
     //The program begins by printing the value of accumulation and then creates a child process using fork.
@@ -19,22 +43,26 @@ int main(){
             perror("fork failed");
             exit (EXIT_FAILURE);
         case 0:
-            //this is child
-            //The child accumulates the values from 1 to N = 10 into accumulation and exits.
-
-            for(int i = 1; i<=10; i++ ){
-                accumulation += i;
-            }
-            //TODO:DEBUG
-            //printf("|DEBUG: %ld|\n", accumulation);
-
+            //this is child-Process
+            accumulation = accumulate(accumulation);
             exit(EXIT_SUCCESS);
-            break;
 
         default:
             //this is parent
+            //The child accumulates the values from 1 to N = 10 into accumulation and exits. The parent waits for the child to exit and prints the value again.
             wait(NULL);
             printf("2: accumulation = %ld\n", accumulation);
+
+            //Next, the program (now referred to as the main thread) spawns a single POSIX thread.
+            pthread_t pthreadID = 0;
+            int pthread_create_retVal = pthread_create(&pthreadID, NULL, pthreadStartRoutine, &accumulation);
+            pthread_error_funct(pthread_create_retVal);
+
+            //The main thread waits for the thread to finish, and prints the value one more time.
+            int pthread_join_retVal = pthread_join(pthreadID, NULL);    //todo: testing with NULL -> else check manpage
+            pthread_error_funct(pthread_join_retVal);
+            printf("3: accumulation = %ld\n", accumulation);
+
     }
 
     return EXIT_SUCCESS;
