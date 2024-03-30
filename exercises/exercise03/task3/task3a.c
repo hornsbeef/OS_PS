d7d4c7dc119928ed4466a3f70d32118b60f3f37e
hornsbeef <login.horninger@gmail.com>
2024-03-30 20:32:24 +0100

    finishes task3a.c

--- exercises/exercise03/task3/task3a.c
+++ exercises/exercise03/task3/task3a.c
@@ -4,35 +4,67 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+
+//todo: testing -> change to 1 before commit
+#define AMOUNT_THREADS 1
+
 
 //Write a program that initializes a global variable accumulation of type int64_t and initializes it to 0.
 int64_t accumulation = 0;
 
-int64_t accumulate(int64_t global_acc) {
+
+//for mutex locking:
+pthread_mutex_t lock;
+
+//pthread ID array:
+pthread_t tid[AMOUNT_THREADS];
+
+
+
+int64_t accumulate_fkt() {
+    int64_t acc = 0;
     //The child accumulates the values from 1 to N = 10 into accumulation and exits.
     for(int i = 1; i <= 10; i++ ){
-        global_acc += i;
+        acc += i;
+        //testing -> remove before commit
+        //printf("%lu: %ld %ld\n",pthread_self(), accumulation, acc);
     }
-    return global_acc;
+    return acc;
 }
 
 void* pthreadStartRoutine(void* void_ptr){
-    //The thread does the same thing as the child process, i.e. accumulate the values from 1to N = 10 into the global variable and immediately exit.
-    accumulation = accumulate(accumulation);
+    //The thread does the same thing as the child process, i.e. accumulate_fkt the values from 1to N = 10 into the global variable and immediately exit.
+
+    pthread_mutex_lock(&lock);      //"take key and lock room"
+
+    accumulation = accumulate_fkt();     //"do stuff in the room"
+    //testing mutex:
+    //accumulation += accumulate_fkt();
+
+    pthread_mutex_unlock(&lock);    //"unlock room; exit room; put key back"
+
     pthread_exit(NULL);
-    return NULL;
 }
 
 void pthread_error_funct(int pthread_returnValue) {
     if(pthread_returnValue != 0){
-        fprintf(stderr, "pthread creation failed! Error code: %d"
-                        "\nNote that the pthreads functions do not set errno.",
-                        pthread_returnValue);
+        char* error_msg = strerror(pthread_returnValue);
+        fprintf(stderr, "Error code: %d\n"
+                        "Error message: %s\n"
+                        "Note that the pthreads functions do not set errno.\n",
+                        pthread_returnValue, error_msg);
         exit(EXIT_FAILURE);
     }
 }
 
 int main(){
+    //for mutex locking:
+    int mutex_init_retVal = pthread_mutex_init(&lock, NULL);
+    pthread_error_funct(mutex_init_retVal);
+
+
 
     //The program begins by printing the value of accumulation and then creates a child process using fork.
     printf("1: accumulation = %ld\n", accumulation);
@@ -44,7 +76,10 @@ int main(){
             exit (EXIT_FAILURE);
         case 0:
             //this is child-Process
-            accumulation = accumulate(accumulation);
+            accumulation = accumulate_fkt();
+            //todo: testing
+            //accumulate_fkt();
+
             exit(EXIT_SUCCESS);
 
         default:
@@ -54,17 +89,46 @@ int main(){
             printf("2: accumulation = %ld\n", accumulation);
 
             //Next, the program (now referred to as the main thread) spawns a single POSIX thread.
-            pthread_t pthreadID = 0;
-            int pthread_create_retVal = pthread_create(&pthreadID, NULL, pthreadStartRoutine, &accumulation);
-            pthread_error_funct(pthread_create_retVal);
+            //pthread_t pID = 0;    //initialize with 0
+
+            int i = 0;
+            int error;
+            while(i < AMOUNT_THREADS){
+                error = pthread_create(&tid[i], NULL, &pthreadStartRoutine, NULL);
+                pthread_error_funct(error);
+                i++;
+            }
+            //int pthread_create_retVal = pthread_create(&pID, NULL, pthreadStartRoutine, &accumulation);
+
+
+
 
             //The main thread waits for the thread to finish, and prints the value one more time.
-            int pthread_join_retVal = pthread_join(pthreadID, NULL);    //todo: testing with NULL -> else check manpage
-            pthread_error_funct(pthread_join_retVal);
+            int j = 0;
+            while(j < AMOUNT_THREADS){
+                //int pthread_join_retVal =
+                pthread_join(tid[j], NULL);    //todo: testing with NULL -> else check manpage
+                //thread_error_funct(pthread_join_retVal);
+                j++;
+            }
+
             printf("3: accumulation = %ld\n", accumulation);
 
     }
 
+    //mutex cleanup
+    pthread_mutex_destroy(&lock);
+
     return EXIT_SUCCESS;
 
 }
+
+//What do you notice? Explain the behavior.
+//the accumulation with the child-PROCESS results in 0 in the global variable, because the value is saved to
+//the variable in the child Process not the parent process.
+// the child thread on the other hand can access the global variable "accumulation" -> therefore changes the value there
+// -> the parent process.
+
+//Make sure to check for possible errors that might occur in the program.
+// -> looks for error in fork and pthread_functions.
+// Look at return codes of functions like pthread_create and print meaningful error messages to standard error using fprintf
