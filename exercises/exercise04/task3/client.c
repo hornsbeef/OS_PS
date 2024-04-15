17342345f869411a1198add46f8486c2f82d5952
hornsbeef <login.horninger@gmail.com>
2024-04-15 02:17:23 +0200

    update to server.c and client.c

--- exercises/exercise04/task3/client.c
+++ exercises/exercise04/task3/client.c
@@ -1,3 +1,66 @@
-//
-// Created by WindOShornsbeef on 14/04/2024.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <linux/limits.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: ."__FILE__ " <client name>");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Now opening file - Please wait.\n");
+    fflush(stdout);
+    errno = 0;
+    int pipe_fd= open(argv[1], O_WRONLY);
+    //because no O_NONBLOCK can be used the program waits here until it can open the file without informing the user.
+    if(pipe_fd < 0){
+        perror("Open failed");
+        exit(EXIT_FAILURE);
+    }else{
+        printf("Sucessfully opened file.\n");
+        fflush(stdout);
+    }
+
+
+    bool cycle = true;
+    char input[PIPE_BUF];   //this is why it is important to know how long the input is going to be.
+
+    while(cycle){
+        printf("Expression: ");
+        //int n_scanned = scanf("%s\n", input );
+        fgets(input, PIPE_BUF, stdin);
+
+        //todo: debugging only:
+        fprintf(stdout, "this was entered:%s\n", input);
+        fflush(stdout);
+
+
+
+        if (strcmp(input, "\n")==0){
+                //nothing was entered
+                //todo: close the connection & break the loop
+                close(pipe_fd);
+                cycle = false;
+                break;
+        }else{
+            //something was entered -> send to pipe, content must be handled by server.
+            write(pipe_fd, input, strlen(input));
+        }
+
+    }
+
+
+    fprintf(stderr, "EXITING...");
+    fflush(stderr);
+    exit(EXIT_SUCCESS);
+
+
+}
+
+
--- exercises/exercise04/task3/server.c
+++ exercises/exercise04/task3/server.c
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <poll.h>
+#include <linux/limits.h>
 
 typedef struct client {
     char name[256];
@@ -20,8 +22,6 @@ void pipe_error_check(int pipe_error);
 void close_all_pipes(int argc);
 
 int main(int argc, char* argv[]) {
-    //close_all_pipes(argc);    //for debugging to close pipes
-
     if (argc < 2) {
         fprintf(stderr, "usage: ."__FILE__ " <client names>");
         exit(EXIT_FAILURE);
@@ -29,6 +29,7 @@ int main(int argc, char* argv[]) {
 
     client_t clients[argc];
     mode_t client_server_fifo_perm = (S_IRUSR | S_IWUSR);
+    struct pollfd fds[argc-1];    //for poll()
 
     //creating all the clients data
     for (int i = 1; i < argc; ++i) {
@@ -40,31 +41,104 @@ int main(int argc, char* argv[]) {
         errno = 0;
         int fifo_error = mkfifo(clients[i].name, client_server_fifo_perm);
         pipe_error_check(fifo_error);
-        fprintf(stderr, "%s : makefifo error code = %d\n", clients[i].name, fifo_error);
+        //fprintf(stderr, "%s : makefifo error code = %d\n", clients[i].name, fifo_error);
+
+
+        //for ease of use without the O_NONBLOCK:
+        fprintf(stdout, "Please connect client %s.\n", clients[i].name);
+        fflush(stdout);
 
         errno = 0;
-        clients[i].file_descriptor = open(clients[i].name, O_RDONLY | O_NONBLOCK);  //maybe O_NONBLOCK is stupid here...
-        fprintf(stderr, "%s : file_descriptor = %d\n", clients[i].name, clients[i].file_descriptor);
+        clients[i].file_descriptor = open(clients[i].name, O_RDONLY);
+
         if(clients[i].file_descriptor < 0){
             perror("Open failed");
         }
+        //unfortunately O_NONBLOCK is not allowed.
+        //So the program waits here until the specified client is connected.
+        fflush(stdout);
+        clients[i].is_connected = true;
+
+        fds[i-1].fd = clients[i].file_descriptor;     //for poll()
+        fds[i-1].events = POLLIN;        //polling here interested in: POLLIN:
+        fprintf(stdout, "%s connected.\n", clients[i].name);
 
     }
 
+    //todo: DEBUG
+    fprintf(stdout, "all clients have been connected\n");
 
 
 
-    //todo: check if clients have connected: maybe use access(fifoPath, F_OK)
+
+    char buf[PIPE_BUF];
+
+
+    int all_clients_disconnected = 0;
+    while(all_clients_disconnected != (argc-1)){
+        printf("waiting to receive data...\n");
+        fflush(stdout);
+
+        //wait indefinitly for events:
+        int events = poll(fds, argc-1, -1);
+
+        for (int i = 1; i < argc; ++i) {    //go through all fd to check which one has data available:
+            if(fds[i-1].revents & POLLHUP){
+                all_clients_disconnected++;
+                printf("%s disconnected.", clients[i].name);    //TODO: why is this not printed????
+                fflush(stdout);
+                //continue; or goto poll ??
+            }
+            if(fds[i-1].revents & POLLIN) {
+                memset(buf, 0, PIPE_BUF);
+                read(clients[i].file_descriptor,buf, PIPE_BUF);
+                fprintf(stdout, "%s was received\n", buf);
+            }
+
+
+        }
+    }
+
+
+    // close all the pipes again
+    for (int all_the_pipes = 0; all_the_pipes < argc; ++all_the_pipes) {
+        close(all_the_pipes);
+        unlink(clients[all_the_pipes].name);            //<- MUST NOT FORGET!
+    }
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
+void pipe_error_check(int pipe_error) {
+    if(pipe_error < 0){
+        //todo: set errno = 0 before pipe() call!
+        perror("Pipe failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+/*  Code for usage with open(fd, O_NONBLOCK) for waiting for clients to connect.
 
     int all_clients_connected = 0;
-    while(all_clients_connected != argc){
-        for (int i = 0; i < argc; ++i) {
+    while(all_clients_connected != (argc-1)){
+        for (int i = 1; i < argc; ++i) {
+            fds[i].events = POLLIN;        //polling here interested in: POLLIN:
+
             if(clients[i].is_connected){
                 continue;
             }else
-            if(access(clients[i].name, F_OK) == 0){
+            if(poll(&fds[i], 1, 1)> 0){     //little workaround with nfds 1 and &fds[i] ... maybe it works
                 all_clients_connected++;
                 clients[i].is_connected = true;
+                fprintf(stdout, "%s connected.\n", clients[i].name);
+                fflush(stdout);
+                if(all_clients_connected == (argc-1)){
+                    goto after_loop;    //goto is not nice, but useful here to break out of 2 loops
+                }
             }
         }
         fprintf(stdout, "waiting for all clients to connect");
@@ -80,30 +154,9 @@ int main(int argc, char* argv[]) {
         fflush(stdout);
         usleep(3500000);
 
-
     }
-    //todo: DEBUG
-    fprintf(stderr, "this should not be printed until all clients have been connected");
+    after_loop:    //for goto: to jump out of 2 loops...
 
 
 
-    usleep(100000);
-
-    //todo: close all the pipes again:
-    close_all_pipes(argc);
-    return EXIT_SUCCESS;
-}
-
-void close_all_pipes(int argc) {
-    for (int all_the_pipes = 0; all_the_pipes < argc; ++all_the_pipes) {
-        close(all_the_pipes);
-    }
-}
-
-void pipe_error_check(int pipe_error) {
-    if(pipe_error < 0){
-        //todo: set errno = 0 before pipe() call!
-        perror("Pipe failed");
-        exit(EXIT_FAILURE);
-    }
-}
+ */
