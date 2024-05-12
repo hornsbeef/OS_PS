d5f8995184daa44c7f9c391d11679e787907bf11
hornsbeef <login.horninger@gmail.com>
2024-05-12 02:28:16 +0200

    exercise07 task3 started

--- exercises/exercise07/task3/thread_pool.c
+++ exercises/exercise07/task3/thread_pool.c
@@ -1,17 +1,37 @@
 #include "thread_pool.h"
 
+/***
+ * The void pool_create(thread_pool* pool, size_t size) function
+ * initializes a thread_pool by starting size worker threads
+ * and initializing a queue for jobs.
+ * Each worker thread continuously checks the queue for submitted jobs.
+ * Whenever a job is available, (exactly) one worker thread removes it from the queue and runs it.
+ */
 void pool_create(thread_pool* pool, size_t size){
 #error "TODO"
 }
 
+/**
+ *The job_id pool_submit(thread_pool* pool, job_function start_routine, job_arg arg)
+ * submits a job to the thread pool
+ * and returns a job_id.
+ */
 job_id pool_submit(thread_pool* pool, job_function start_routine, job_arg arg) {
 #error "TODO"
 }
 
+/**
+ * The void pool_await(job_id id) function waits for the job with the given job_id to finish.
+ */
 void pool_await(job_id id) {
 #error "TODO"
+    //https://www.perplexity.ai/search/typedef-void-jobfunctionvoid-j9g0hq9XS1KLJ9f4WVHvXw
 }
 
+/**
+ * The void pool_destroy(thread_pool* pool) shuts down the thread pool and frees all associated resources.
+ * Worker threads finish the currently running job (if any) and then stop gracefully.
+ */
 void pool_destroy(thread_pool* pool) {
 #error "TODO"
 }
--- exercises/exercise07/task3/thread_pool.h
+++ exercises/exercise07/task3/thread_pool.h
@@ -2,33 +2,78 @@
 #define THREAD_POOL_H
 
 #include <stddef.h> // for size_t
+#include <stdint.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/queue.h> // see queue(7) & stailq(3)
 
-typedef void* (*job_function)(void*);
-typedef void* job_arg;
-typedef /* TODO */ job_id;
+typedef void* (*job_function)(void *);
+typedef void *job_arg;
+
+//typedef /* TODO */ job_id;
+typedef uint64_t job_id;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 /***
  * This is the stub of a simple job queue.
  */
-
 struct job_queue_entry {
-	// TODO: Design the contents for a queue, which stores jobs.
+    // TODO: Design the contents for a queue, which stores jobs.
+    int value;
+    //todo: check if additional parameter for job_id is necessary??
+    STAILQ_ENTRY(job_queue_entry) entries;
 };
+
+STAILQ_HEAD(myqueue_head, job_queue_entry);
+
+typedef struct myqueue_head myqueue;
+
+static void myqueue_init(myqueue* q) {
+    STAILQ_INIT(q);
+}
+
+static bool myqueue_is_empty(myqueue* q) {
+    return STAILQ_EMPTY(q);
+}
+
+static void myqueue_push(myqueue* q, int value) {
+    struct job_queue_entry* entry = malloc(sizeof(struct job_queue_entry));
+    entry->value = value;
+    STAILQ_INSERT_TAIL(q, entry, entries);
+}
+
+static int myqueue_pop(myqueue* q) {
+    assert(!myqueue_is_empty(q));
+    struct job_queue_entry* entry = STAILQ_FIRST(q);
+    const int value = entry->value;
+    STAILQ_REMOVE_HEAD(q, entries);
+    free(entry);
+    return value;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 /***
  * This is the stub for the thread pool that uses the queue.
  * Implement at LEAST the Prototype functions below.
  */
-
 typedef struct {
-	// TODO: Design the contents of a thread pool
+    // TODO: Design the contents of a thread pool
 } thread_pool;
 
+
 // Prototypes for REQUIRED functions
-void pool_create(thread_pool* pool, size_t size);
-job_id pool_submit(thread_pool* pool, job_function start_routine,
-                   job_arg arg); // You need to define a datatype for the job_id (chose wisely).
+void pool_create(thread_pool *pool, size_t size);
+
+job_id pool_submit(thread_pool *pool, job_function start_routine, job_arg arg);
+
+// You need to define a datatype for the job_id (chose wisely).
 void pool_await(job_id id);
-void pool_destroy(thread_pool* pool);
+
+void pool_destroy(thread_pool *pool);
 
 #endif
