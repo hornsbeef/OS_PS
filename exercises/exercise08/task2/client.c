5cd1770a446462e78b4a82803a29fd5e31ff47be
hornsbeef <login.horninger@gmail.com>
2024-05-20 01:14:59 +0200

    exercise08/task2 most ugly implementation of client.c and server.c check for 2 decimal places in double is not quite working yet

---
+++ exercises/exercise08/task2/client.c
@@ -0,0 +1,215 @@
+#define _POSIX_C_SOURCE 199309L
+#define _DEFAULT_SOURCE
+// #define _BSD_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <setjmp.h>
+#include <pthread.h>
+#include <signal.h>
+
+
+void check_argc(int argc);
+unsigned long long int cast_to_ulli_with_check(char *string);
+double cast_to_double_with_check(char* buf, jmp_buf msg_rev_loop) ;
+
+
+// ! no global variables must be used -> no idea how to handle signals without a global flag.
+volatile sig_atomic_t interrupted = 0;
+
+void sigint_handler(int sig) {
+    (void) sig;
+    interrupted = 1;
+}
+
+
+// Function to handle receiving messages from the server
+void *listener_thread(void *arg) {
+    int sockfd = *(int *)arg;
+
+    while (1) {
+        char buffer[1024];
+        ssize_t bytes_received = recv(sockfd, buffer, sizeof(buffer), 0);
+        if (bytes_received > 0) {
+            buffer[bytes_received] = '\0';
+            printf("Donations: %s\n", buffer);
+        } else if (bytes_received == 0) {
+            printf("Server disconnected.\n");
+            interrupted = 1;        // * handle cleanup!
+            pthread_exit(NULL);
+        } else {
+            perror("Error receiving data");
+            interrupted = 1;        // * handle cleanup!
+            pthread_exit(NULL);
+        }
+    }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    //Region check args
+    int return_param = EXIT_SUCCESS;
+
+    check_argc(argc);
+
+    unsigned long long port = cast_to_ulli_with_check(argv[1]);
+    if(port < 1024 || port >65535){
+        printf("usage: ."__FILE__" < 1 port between 1024 - 65535 >\n");
+        exit(EXIT_FAILURE);
+    }
+    //End
+
+    //Region socket init
+
+    errno = 0;
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);   //protocol: 0 for default for this , 6 for
+    if (sockfd < 0) {
+        perror("Error creating socket");
+        return EXIT_FAILURE;
+    }
+    int opt = 1; // * set SO_REUSEADDR to value of opt
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("Setsockopt failed");
+        goto cleanup1;
+    }
+
+    const struct sockaddr_in addr = {       //posted struct did NOT work
+            .sin_family = AF_INET,
+            //.sin_addr = htonl(INADDR_ANY),
+            .sin_addr = htonl(INADDR_LOOPBACK), // on client
+            .sin_port = htons(port),
+    };
+
+    //End
+
+    //Region connect:
+    if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("Connection failed");
+        goto cleanup1;
+    }
+    //End
+
+    //Region signal handler
+
+    // catch sigint to clean up
+    struct sigaction act = {0};
+    act.sa_handler = &sigint_handler;
+    sigfillset(&act.sa_mask);   //all signals blocked while in signal handler
+    sigaction(SIGTERM, &act, NULL);
+    sigaction(SIGINT, &act, NULL);
+
+    //End
+
+    //Region spawn thread:
+
+    pthread_t listener_tid;
+    if (pthread_create(&listener_tid, NULL, listener_thread, &sockfd) != 0) {
+        perror("Failed to create listener thread");
+        goto cleanup1;
+    }
+    //End
+
+    //Region work:
+    /* *
+     * Note that you don't need to use ntohl() or ntohs() here because the recv() function
+     * automatically converts the received data from network byte order to host byte order.
+     */
+
+
+    while(interrupted == 0){        // *: need to implement CTRL + C handling
+        char buffer_from_server[1024] = {0};
+        char buffer_input[1024] = {0};
+        if(fgets(buffer_input, sizeof(buffer_input) - sizeof(char), stdin))    // * terminates input with \0
+        {
+            if (ferror(stdin)) {
+                fprintf(stderr, "Error reading input: %s\n", strerror(errno));
+            } else if (feof(stdin)) {
+                fprintf(stderr, "End of file reached\n");
+            }
+        }
+
+        //fprintf(stderr, "buffer_input = %s", buffer_input);
+
+        if(strcmp(buffer_input, "/quit\n") == 0){
+            goto cleanup2;
+        }
+
+        send(sockfd, buffer_input, strlen(buffer_input), 0);    // * 0 means no special flags are set
+
+        // * replace \n with \0 in char buffer[]
+        //buffer_input[strcspn(buffer_input, "\n")] = '\0';
+        if(strcmp(buffer_input, "/shutdown\n") == 0 ){
+            goto cleanup2;
+        }
+
+
+        //int bytes_received = recv(sockfd, buffer_from_server, sizeof(buffer_from_server), 0);
+        //if (bytes_received > 0) {
+        //    buffer_from_server[bytes_received] = '\0';      // * set last char to \0
+        //    printf("Donations: %s\n", buffer_from_server);   //TODO:  Change to required format
+        //}
+
+
+    }
+
+
+
+
+
+    //Region cleanup
+    //cleanup2:
+    //close(conn_sockfd);
+    //printf("Connection closed.\n");
+
+    cleanup2:
+    pthread_cancel(listener_tid);
+    pthread_join(listener_tid, NULL);
+
+    cleanup1:
+    close(sockfd);
+    exit(return_param);
+
+    //End
+
+
+}
+
+//Region helperfunctions
+
+void check_argc(int argc) {
+    if (argc != 2) {
+        printf("usage: ."__FILE__" <port>");
+        exit(EXIT_FAILURE);
+    }
+}
+
+unsigned long long int cast_to_ulli_with_check(char *string) {
+    errno = 0;
+    char *end = NULL;
+    unsigned long long operand = strtoull(string, &end, 10);
+    //check conversion:
+    if ((*end != '\0') || (string == end)) {       //conversion interrupted || no conversion happened
+        fprintf(stderr, "Conversion of argument ended with error.\n");
+        if(errno != 0){
+            perror("StrToULL");
+        }
+        exit(EXIT_FAILURE);
+    }
+    if (errno != 0) {        //== ERANGE //as alternative to != 0
+        perror("Conversion of argument ended with error");
+        fprintf(stderr, "Please recheck usage!\n");
+        exit(EXIT_FAILURE);
+    }
+    return operand;
+}
+
+
+
+
+//End
---
+++ exercises/exercise08/task2/server.c
@@ -0,0 +1,229 @@
+#define _POSIX_C_SOURCE 199309L
+#define _DEFAULT_SOURCE
+// #define _BSD_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <setjmp.h>
+#include <math.h>
+
+
+void check_argc(int argc);
+unsigned long long int cast_to_ulli_with_check(char *string);
+double cast_to_double_with_check(char* buf, jmp_buf msg_rev_loop) ;
+
+
+// ! no global variables must be used -> no idea how to handle signals without a global flag.
+
+int main(int argc, char *argv[]) {
+    //Region check args
+    int return_param = EXIT_SUCCESS;
+    double total_donations = 0;
+
+    check_argc(argc);
+
+    unsigned long long port = cast_to_ulli_with_check(argv[1]);
+    if(port < 1024 || port >65535){
+        printf("usage: ."__FILE__" < 1 port between 1024 - 65535 >\n");
+        exit(EXIT_FAILURE);
+    }
+    //End
+
+    //Region socket init - listen
+
+    errno = 0;
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);   //protocol: 0 for default for this , 6 for
+    if (sockfd < 0) {
+        perror("Error creating socket");
+        return EXIT_FAILURE;
+    }
+    int opt = 1; // * set SO_REUSEADDR to value of opt
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("Setsockopt failed");
+        goto cleanup1;
+    }
+
+
+    const struct sockaddr_in addr = {       //posted struct did NOT work
+            .sin_family = AF_INET,
+            .sin_addr = htonl(INADDR_ANY),
+            //.sin_addr = htonl(INADDR_LOOPBACK),
+            .sin_port = htons(port),
+    };
+
+    //bind is for server; //connect is for client
+    if(bind(sockfd, (const struct sockaddr*) &addr, sizeof(addr) ) != 0 ){
+        perror("Bind");
+        return_param = EXIT_FAILURE;
+        goto cleanup1;
+    }
+
+    // Listen for incoming connections
+    if (listen(sockfd, 1) < 0) {
+        perror("Error listening on socket");
+        return_param = EXIT_FAILURE;
+        goto cleanup1;
+    }
+
+    printf("Listening on port %llu.\n", port);
+
+    //End
+
+    //Region client conn
+/**
+ * In the accept() function in C, the addr and addrlen arguments can be set to NULL when you are not interested
+ * in capturing the address information of the connecting client.
+ * This is commonly done when you only need the new socket descriptor
+ * for communication and do not require details about the client's address.
+ */
+    int conn_sockfd;
+    while (true) {
+        errno = 0;
+        if ((conn_sockfd = accept(sockfd, NULL, NULL)) == -1) {
+            perror("Accept");
+            return_param = EXIT_FAILURE;
+            goto cleanup2;
+        }
+
+        printf("Established connection!\n");
+
+
+        char buffer[1024];
+        jmp_buf msg_rec_loop;
+
+        if(setjmp(msg_rec_loop) == 1){
+            dprintf(conn_sockfd, "%s is not a valid amount.\n", buffer);
+        }
+
+        while(true){        // * continuously receiving msg
+            memset(buffer, '\0', sizeof(*buffer));
+            errno = 0;
+            ssize_t bytes_received = recv(conn_sockfd, buffer, sizeof(buffer), 0);
+            if(bytes_received == -1){
+                perror("Recv");
+                return_param = EXIT_FAILURE;
+                goto cleanup2;
+            }
+            if(bytes_received == 0){    // * When a stream socket peer has performed an orderly shutdown, the return value will be 0 (the traditional "end-of-file" return).
+                printf("Connection closed by client.\n");
+                printf("Waiting for new connection...\n");
+                // * could reset total_donations to 0 here, no idea if I should.
+                break;
+            }
+
+            // * replace \n with \0 in char buffer[]
+            buffer[strcspn(buffer, "\n")] = '\0';
+            if(strcmp(buffer, "/shutdown") == 0){
+                printf("Shutting down.\n"
+                       "Closing connection ...\n");
+                goto cleanup2;
+            }
+            double donation = cast_to_double_with_check(buffer, msg_rec_loop);
+
+
+            total_donations += donation;
+            dprintf(conn_sockfd, "Donations: %.2f\n", total_donations);
+
+        }
+    }
+
+
+
+    //End
+
+    //Region cleanup
+cleanup2:
+    close(conn_sockfd);
+    printf("Connection closed.\n");
+
+cleanup1:
+    close(sockfd);
+    exit(return_param);
+
+    //End
+
+
+}
+
+
+//helper functs
+void check_argc(int argc) {
+    if (argc != 2) {
+        printf("usage: ."__FILE__" < 1 port >= 1024 >");
+        exit(EXIT_FAILURE);
+    }
+}
+
+unsigned long long int cast_to_ulli_with_check(char *string) {
+    errno = 0;
+    char *end = NULL;
+    unsigned long long operand = strtoull(string, &end, 10);
+    //check conversion:
+    if ((*end != '\0') || (string == end)) {       //conversion interrupted || no conversion happened
+        fprintf(stderr, "Conversion of argument ended with error.\n");
+        if(errno != 0){
+            perror("StrToULL");
+        }
+        exit(EXIT_FAILURE);
+    }
+    if (errno != 0) {        //== ERANGE //as alternative to != 0
+        perror("Conversion of argument ended with error");
+        fprintf(stderr, "Please recheck usage!\n");
+        exit(EXIT_FAILURE);
+    }
+    return operand;
+}
+double cast_to_double_with_check(char* buf, jmp_buf msg_rec_loop) {
+
+    //Region another validity check
+    ////char input_checker[1024]
+    //unsigned long dot_value = strcspn(buf, ".");
+    //fprintf(stderr, "%s\n",buf+dot_value);
+    //if(!(buf[dot_value + 3] == '\0' || buf[dot_value + 3] == '\n')){
+    //    fprintf(stderr, "Server: has more than 2 decimal points: %s\n", buf);
+    //    longjmp(msg_rec_loop, 1);
+    //}
+
+
+    //End
+
+    errno = 0;
+    char *end = NULL;
+    double operand = strtod(buf, &end);
+
+    //check conversion:
+    if ((*end != '\0') && *end != EOF) {       //conversion interrupted || no conversion happened
+        //printf("%s is not a valid amount.", buf);
+        longjmp(msg_rec_loop, 1);
+    }
+
+    //Region new validity check:
+    if (operand <= 0) {       // * All donations must be non-negative floating point numbers
+        //fprintf(stderr, "%s is counted as smaller than 0.", buf);
+        longjmp(msg_rec_loop, 1);
+    }
+
+    double fractionalPart, integralPart;
+    fractionalPart = modf(operand, &integralPart);  // Get the fractional part
+
+    fractionalPart *= 100;
+    double fractionalFractionalPart, fractionalIntegralPart;
+    fractionalFractionalPart = modf(fractionalPart, &fractionalIntegralPart);
+    // Check if the fractional part has no more than 2 decimal places
+    if (fractionalFractionalPart > 0)
+    {
+        //fprintf(stderr, "Fractional part contains more than 2 decimal places\n");
+        longjmp(msg_rec_loop, 1);
+    }
+    //End
+
+    //fprintf(stderr, "CLIENT: conversion successful -> return to main\n");
+    return operand;
+
+
+}
