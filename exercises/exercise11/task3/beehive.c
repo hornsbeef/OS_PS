6a3b53a85e51d5f7c3159d5c748e9eef832a8677
hornsbeef <login.horninger@gmail.com>
2024-06-11 04:07:02 +0200

    exercise11 beehive working @home -> test on zid UPDATE --> works on Zid, no valgrind errors --->Makefile updated ----> Auto-formated everything

--- exercises/exercise11/task3/beehive.c
+++ exercises/exercise11/task3/beehive.c
@@ -36,12 +36,14 @@ void pthread_error_funct(int pthread_returnValue) {
 //copied from previous homework (e10) but modified
 #define MIN_ARGS 4
 #define MAX_ARGS 4
+
 void check_argc(int argc) {
     if (argc < MIN_ARGS || argc > MAX_ARGS) {
         printf("usage: ."__FILE__" <flower field width> <flower field height> <number of bees>\n");
         exit(EXIT_FAILURE);
     }
 }
+
 #undef MIN_ARGS
 #undef MAX_ARGS
 
@@ -68,17 +70,17 @@ unsigned long long int cast_to_ulli_with_check(char *string) {
 
 void *pthreadStartRoutine(void *arg) {
     //pthread_args* my_pthread_args_ptr = (pthread_args *) arg;   //casting the void* arg to pthread_args*
-    bee_t* bee_PTR = (bee_t*) arg;
+    bee_t *bee_PTR = (bee_t *) arg;
     unsigned int seed = time(NULL) ^ pthread_self(); //MUST BE DONE BEFORE LOOP! HERE rand_r safes state!
 
-    while(bee_PTR->beehive_PTR->num_nectar < bee_PTR->beehive_PTR->max_number_nectar){
+    while (bee_PTR->beehive_PTR->num_nectar < bee_PTR->beehive_PTR->max_number_nectar) {
         //terminate when all flowers have been collected.
 
         bool work_flower = false;
 
-        flower_t* food_source = NULL;
+        flower_t *food_source = NULL;
         pthread_error_funct(pthread_mutex_lock(bee_PTR->beehive_PTR->mutex_PTR));
-        if(myqueue_is_empty(bee_PTR->beehive_PTR->queue) == false){
+        if (myqueue_is_empty(bee_PTR->beehive_PTR->queue) == false) {
             food_source = myqueue_pop(bee_PTR->beehive_PTR->queue);
             work_flower = true;
         }
@@ -90,119 +92,125 @@ void *pthreadStartRoutine(void *arg) {
         //fprintf(stderr, "random num: %d\n", random_number);
         //relocating the acutal sleep into the different work szenarios
 
-        if(work_flower){
-            if(food_source != NULL) //if food_source is NULL -> something went wrong -> just ignore this cycle (hopefully works)
+        if (work_flower) {
+            if (food_source !=
+                NULL) //if food_source is NULL -> something went wrong -> just ignore this cycle (hopefully works)
             {
-                printf("Bee %llu is flying to food source at position (%llu,%llu).\n", bee_PTR->bee_name, food_source->heigt, food_source->width);
+                printf("Bee %llu is flying to food source at position (%llu,%llu).\n", bee_PTR->bee_name,
+                       food_source->heigt, food_source->width);
                 errno = 0;
-                if(usleep(random_number * 1000) < 0){
+                if (usleep(random_number * 1000) < 0) {
                     perror("Usleep");
                     exit(EXIT_FAILURE);
                 };
 
             }
             //check if flowr is harvestable:
-            if(food_source != NULL && food_source->has_been_harvested == false){
+            if (food_source != NULL && food_source->has_been_harvested == false) {
                 //harvest from flower
                 food_source->has_been_harvested = true;
                 //increase total nectar count
                 bee_PTR->beehive_PTR->num_nectar++;
 
-            //check surroundings -> add to queue;
+                //check surroundings -> add to queue;
                 //create strings for new food source:
                 char left[256] = {"\0"};
                 char right[256] = {"\0"};
                 char up[256] = {"\0"};
                 char down[256] = {"\0"};
                 //left  -> height same
-                if(food_source->width >0 ){
-                    flower_t* nextfl = (bee_PTR->flowerfield_PTR + food_source->heigt * bee_PTR->flowerfield_PTR->ff_width + (food_source->width - 1));
+                if (food_source->width > 0) {
+                    flower_t *nextfl = (bee_PTR->flowerfield_PTR +
+                                        food_source->heigt * bee_PTR->flowerfield_PTR->ff_width +
+                                        (food_source->width - 1));
                     //if((bee_PTR->flowerfield_PTR + food_source->heigt * bee_PTR->flowerfield_PTR->ff_width + (food_source->width - 1))->has_been_harvested == false){
-                    if(nextfl->has_been_harvested == false){
+                    if (nextfl->has_been_harvested == false) {
 
                         //simmilar to previous homeworks that used queues -> as are all other myqueue_pushes /pops
                         pthread_error_funct(pthread_mutex_lock(bee_PTR->beehive_PTR->mutex_PTR));
                         myqueue_push(bee_PTR->beehive_PTR->queue, nextfl);
-                       // fprintf(stderr, "queue empty: %s\n", myqueue_is_empty(bee_PTR->beehive_PTR->queue) ? "true" : "false");
+                        // fprintf(stderr, "queue empty: %s\n", myqueue_is_empty(bee_PTR->beehive_PTR->queue) ? "true" : "false");
                         pthread_error_funct(pthread_mutex_unlock(bee_PTR->beehive_PTR->mutex_PTR));
                         errno = 0;  //https://pubs.opengroup.org/onlinepubs/9699919799/functions/fprintf.html
-                        if(sprintf(left, "(%llu,%llu)", nextfl->heigt, nextfl->width) <0){
+                        if (sprintf(left, "(%llu,%llu)", nextfl->heigt, nextfl->width) < 0) {
                             perror("Sprintf");
                             exit(EXIT_FAILURE);
                         }
-                    }
-                    else{
+                    } else {
                         memset(left, '\0', sizeof(left));
                         //The memset() function shall return s; no return value is reserved
                         //       to indicate an error.
                     }
                 }
                 //right-> height same
-                if(food_source->width < food_source->ff_width -1 ){
-                    flower_t* nextfl = (bee_PTR->flowerfield_PTR + food_source->heigt * bee_PTR->flowerfield_PTR->ff_width + (food_source->width + 1));
+                if (food_source->width < food_source->ff_width - 1) {
+                    flower_t *nextfl = (bee_PTR->flowerfield_PTR +
+                                        food_source->heigt * bee_PTR->flowerfield_PTR->ff_width +
+                                        (food_source->width + 1));
                     //if((bee_PTR->flowerfield_PTR + food_source->heigt * bee_PTR->flowerfield_PTR->ff_width + (food_source->width - 1))->has_been_harvested == false){
-                    if(nextfl->has_been_harvested == false){
+                    if (nextfl->has_been_harvested == false) {
                         pthread_error_funct(pthread_mutex_lock(bee_PTR->beehive_PTR->mutex_PTR));
                         myqueue_push(bee_PTR->beehive_PTR->queue, nextfl);
                         //fprintf(stderr, "queue empty: %s\n", myqueue_is_empty(bee_PTR->beehive_PTR->queue) ? "true" : "false");
                         pthread_error_funct(pthread_mutex_unlock(bee_PTR->beehive_PTR->mutex_PTR));
                         errno = 0;  //https://pubs.opengroup.org/onlinepubs/9699919799/functions/fprintf.html
-                        if(sprintf(right, "(%llu,%llu)", nextfl->heigt, nextfl->width) <0){
+                        if (sprintf(right, "(%llu,%llu)", nextfl->heigt, nextfl->width) < 0) {
                             perror("Sprintf");
                             exit(EXIT_FAILURE);
                         }
-                    }
-                    else{
+                    } else {
                         memset(right, '\0', sizeof(right));
                     }
                 }
                 //up -> width same
-                if(food_source->heigt >0 ){
-                    flower_t* nextfl = (bee_PTR->flowerfield_PTR + (food_source->heigt -1)* bee_PTR->flowerfield_PTR->ff_width + (food_source->width));
+                if (food_source->heigt > 0) {
+                    flower_t *nextfl = (bee_PTR->flowerfield_PTR +
+                                        (food_source->heigt - 1) * bee_PTR->flowerfield_PTR->ff_width +
+                                        (food_source->width));
                     //if((bee_PTR->flowerfield_PTR + food_source->heigt * bee_PTR->flowerfield_PTR->ff_width + (food_source->width - 1))->has_been_harvested == false){
-                    if(nextfl->has_been_harvested == false){
+                    if (nextfl->has_been_harvested == false) {
                         pthread_error_funct(pthread_mutex_lock(bee_PTR->beehive_PTR->mutex_PTR));
                         myqueue_push(bee_PTR->beehive_PTR->queue, nextfl);
                         //fprintf(stderr, "queue empty: %s\n", myqueue_is_empty(bee_PTR->beehive_PTR->queue) ? "true" : "false");
                         pthread_error_funct(pthread_mutex_unlock(bee_PTR->beehive_PTR->mutex_PTR));
 
                         errno = 0;  //https://pubs.opengroup.org/onlinepubs/9699919799/functions/fprintf.html
-                        if(sprintf(up, "(%llu,%llu)", nextfl->heigt, nextfl->width) <0){
+                        if (sprintf(up, "(%llu,%llu)", nextfl->heigt, nextfl->width) < 0) {
                             perror("Sprintf");
                             exit(EXIT_FAILURE);
                         }
-                    }
-                    else{
+                    } else {
                         memset(up, '\0', sizeof(up));
                     }
                 }
                 //down -> width same
-                if(food_source->heigt < food_source->ff_height -1 ){
-                    flower_t* nextfl = (bee_PTR->flowerfield_PTR + (food_source->heigt + 1)* bee_PTR->flowerfield_PTR->ff_width + (food_source->width));
+                if (food_source->heigt < food_source->ff_height - 1) {
+                    flower_t *nextfl = (bee_PTR->flowerfield_PTR +
+                                        (food_source->heigt + 1) * bee_PTR->flowerfield_PTR->ff_width +
+                                        (food_source->width));
                     //if((bee_PTR->flowerfield_PTR + food_source->heigt * bee_PTR->flowerfield_PTR->ff_width + (food_source->width - 1))->has_been_harvested == false){
-                    if(nextfl->has_been_harvested == false){
+                    if (nextfl->has_been_harvested == false) {
                         pthread_error_funct(pthread_mutex_lock(bee_PTR->beehive_PTR->mutex_PTR));
                         myqueue_push(bee_PTR->beehive_PTR->queue, nextfl);
-                       // fprintf(stderr, "queue empty: %s\n", myqueue_is_empty(bee_PTR->beehive_PTR->queue) ? "true" : "false");
+                        // fprintf(stderr, "queue empty: %s\n", myqueue_is_empty(bee_PTR->beehive_PTR->queue) ? "true" : "false");
                         pthread_error_funct(pthread_mutex_unlock(bee_PTR->beehive_PTR->mutex_PTR));
                         errno = 0;  //https://pubs.opengroup.org/onlinepubs/9699919799/functions/fprintf.html
-                        if(sprintf(down, "(%llu,%llu)", nextfl->heigt, nextfl->width) <0){
+                        if (sprintf(down, "(%llu,%llu)", nextfl->heigt, nextfl->width) < 0) {
                             perror("Sprintf");
                             exit(EXIT_FAILURE);
                         }
-                    }
-                    else{
+                    } else {
                         memset(down, '\0', sizeof(down));
                     }
                 }
 
                 //fprintf(stderr, "DONE CHECKING SURROUNDINGS \n\n\n");
                 char comp[256] = {"\0"};
-                if(strcmp(comp, left) ==0 && strcmp(comp, right) ==0 && strcmp(comp, up) ==0 && strcmp(comp, down)== 0){
+                if (strcmp(comp, left) == 0 && strcmp(comp, right) == 0 && strcmp(comp, up) == 0 &&
+                    strcmp(comp, down) == 0) {
                     printf("Bee %llu collected nectar at position (%llu,%llu) and reports no other food sources.\n",
                            bee_PTR->bee_name, food_source->heigt, food_source->width);
-                }
-                else{
+                } else {
                     printf("Bee %llu collected nectar at position (%llu,%llu) and reports potential food sources: %s%s%s%s.\n",
                            bee_PTR->bee_name, food_source->heigt, food_source->width,
                            left, right, up, down);
@@ -213,10 +221,10 @@ void *pthreadStartRoutine(void *arg) {
             work_flower = false;
             //return home;
         }
-        //else working at home
-        else{
+            //else working at home
+        else {
             printf("Bee %llu is working in beehive.\n", bee_PTR->bee_name);
-            if(usleep(random_number * 1000) < 0){
+            if (usleep(random_number * 1000) < 0) {
                 perror("Usleep");
                 exit(EXIT_FAILURE);
             };
@@ -227,29 +235,28 @@ void *pthreadStartRoutine(void *arg) {
         //possible bear attack:
         //simmilar to homework(e10) about barriers
         int ret = pthread_barrier_wait(bee_PTR->beehive_PTR->barrier_PTR);
-        if (ret == 0) { goto nextwait; }    //All but 1 thread: go to next pthread_barrier_wait so all threads are at the same location again.
+        if (ret ==
+            0) { goto nextwait; }    //All but 1 thread: go to next pthread_barrier_wait so all threads are at the same location again.
         else if (ret == PTHREAD_BARRIER_SERIAL_THREAD) // the ONE thread:
         {
             int random_chance = rand_r(&seed) % (100 - 0 + 1) + 1;
             //fprintf(stderr, "random chance: %d\n"
             //                "calculated by thread: %lu\n\n", random_chance, pthread_self());
-            if(random_chance <= 10){
+            if (random_chance <= 10) {
                 //bear attack occurs:
                 printf("Bees encounter a bear and engage in a fight.\n");
                 int random_chance2 = rand_r(&seed) % (100 - 0 + 1) + 1;
-                if(random_chance2<=50){
+                if (random_chance2 <= 50) {
                     //bear destorys nest -> end of story
                     bee_PTR->beehive_PTR->actual_number_of_nectar_collected = bee_PTR->beehive_PTR->num_nectar;
                     bee_PTR->beehive_PTR->destroyed_by_bear = true;
                     bee_PTR->beehive_PTR->num_nectar = bee_PTR->beehive_PTR->max_number_nectar;
                     printf("Bear destroys the beehive.\n");
-                }
-                else{
+                } else {
                     printf("The bees successfully repel the bear and resume their work.\n");
                 }
             }
-        }
-        else /*this means error in pthread_barrier_wait() */{
+        } else /*this means error in pthread_barrier_wait() */{
             pthread_error_funct(ret);
         }
 
@@ -270,11 +277,11 @@ int main(int argc, char *argv[]) {
 
     unsigned long long ff_width = cast_to_ulli_with_check(argv[1]);
     unsigned long long ff_height = cast_to_ulli_with_check(argv[2]);
-    unsigned long long  num_bees = cast_to_ulli_with_check(argv[3]);
+    unsigned long long num_bees = cast_to_ulli_with_check(argv[3]);
 
     flower_t flowerfield[ff_height][ff_width];
-    for (unsigned long long i = 0; i < ff_height; i++){
-        for (unsigned long long j = 0; j < ff_width; j++){
+    for (unsigned long long i = 0; i < ff_height; i++) {
+        for (unsigned long long j = 0; j < ff_width; j++) {
             flowerfield[i][j].has_been_harvested = false;
             flowerfield[i][j].ff_height = ff_height;
             flowerfield[i][j].ff_width = ff_width;
@@ -324,10 +331,10 @@ int main(int argc, char *argv[]) {
 
     i_dont_trust_myself_to_get_the_random_number_between_the_array_bounds:
     unsigned int seed = time(NULL) ^ pthread_self();
-    unsigned long long random_height = rand_r(&seed) % ((ff_height-1) - 0 + 1) + (0);
-    unsigned long long random_width = rand_r(&seed) % ((ff_width-1) - 0 + 1) + (0);
+    unsigned long long random_height = rand_r(&seed) % ((ff_height - 1) - 0 + 1) + (0);
+    unsigned long long random_width = rand_r(&seed) % ((ff_width - 1) - 0 + 1) + (0);
     //fprintf(stderr, "TESTING: %d", random_height);
-    if(random_height > ff_height || random_width > ff_width){
+    if (random_height > ff_height || random_width > ff_width) {
         //this could create a very long loop time, but it should at least prevent an array_out_of_bounds problem.
         goto i_dont_trust_myself_to_get_the_random_number_between_the_array_bounds;
     }
@@ -341,18 +348,18 @@ int main(int argc, char *argv[]) {
     //create ptheads = bees
     //simmilar to all previous homeworks containing pthreads.
     pthread_t pid_array[num_bees];
-    for(unsigned long long i = 0; i < num_bees; i++){
+    for (unsigned long long i = 0; i < num_bees; i++) {
         //set beehive & flowerfield PTR
         all_bees_array[i].beehive_PTR = &beehive;
         all_bees_array[i].flowerfield_PTR = &flowerfield[0][0];
         all_bees_array[i].bee_name = i;
         //// Access elements using *(flowrefield_PTR + i * ff_width + j)
-        pthread_error_funct(pthread_create(&pid_array[i],NULL, &pthreadStartRoutine, &all_bees_array[i] ));
+        pthread_error_funct(pthread_create(&pid_array[i], NULL, &pthreadStartRoutine, &all_bees_array[i]));
 
     }
 
     //simmilar to all previous homeworks containing pthreads.
-    for(unsigned long long i = 0; i< num_bees; i++){
+    for (unsigned long long i = 0; i < num_bees; i++) {
         pthread_error_funct(pthread_join(pid_array[i], NULL));
     }
 
@@ -365,20 +372,20 @@ int main(int argc, char *argv[]) {
     //    }
     //}
 
-    if(beehive.destroyed_by_bear == true){
-        printf("%llu bees collected nectar from %d/%llu flowers.\n", num_bees, beehive.actual_number_of_nectar_collected, (ff_height*ff_width));
+    if (beehive.destroyed_by_bear == true) {
+        printf("%llu bees collected nectar from %d/%llu flowers.\n", num_bees,
+               beehive.actual_number_of_nectar_collected, (ff_height * ff_width));
         printf("Beehive was destroyed.\n");
-    }
-    else{
+    } else {
         printf("The bees have collected all the nectar form the flowerfield.\n");
         printf("The beehive was NOT destroyed.\n   ");
     }
 
-    while(myqueue_is_empty(beehive.queue) == false){
+    while (myqueue_is_empty(beehive.queue) == false) {
         myqueue_pop(beehive.queue); //getting rid of all still stored jobs
     }
 
 
-    return(EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 
 }
--- exercises/exercise11/task3/myqueue.h
+++ exercises/exercise11/task3/myqueue.h
@@ -13,56 +13,56 @@ typedef struct flower {
     atomic_bool has_been_harvested;
     unsigned long long ff_height;
     unsigned long long ff_width;
-    unsigned long long  heigt;
-    unsigned long long  width;
-}flower_t;
+    unsigned long long heigt;
+    unsigned long long width;
+} flower_t;
 
 typedef struct beehive {
-    myqueue* queue;
+    myqueue *queue;
     atomic_int max_number_nectar;
     atomic_int num_nectar;
     atomic_int actual_number_of_nectar_collected; //used in case of bear attack
     atomic_bool destroyed_by_bear;
-    pthread_mutex_t* mutex_PTR;
-    pthread_barrier_t* barrier_PTR;
+    pthread_mutex_t *mutex_PTR;
+    pthread_barrier_t *barrier_PTR;
 
-}beehive_t;
+} beehive_t;
 
-typedef struct bee{
-    flower_t* flowerfield_PTR;  // Access elements using *(flowrefield_PTR + i * ff_width + j)
-    beehive_t* beehive_PTR;
+typedef struct bee {
+    flower_t *flowerfield_PTR;  // Access elements using *(flowrefield_PTR + i * ff_width + j)
+    beehive_t *beehive_PTR;
     unsigned long long bee_name;
-}bee_t;
+} bee_t;
 
 
 struct myqueue_entry {
     //int value;  //depends on this, what can be pushed into the queue
-    flower_t* flower_PTR;
+    flower_t *flower_PTR;
     STAILQ_ENTRY(myqueue_entry) entries;
 };
 
 STAILQ_HEAD(myqueue_head, myqueue_entry);
 
-static void myqueue_init(myqueue* q) {
+static void myqueue_init(myqueue *q) {
     STAILQ_INIT(q);
 }
 
-static bool myqueue_is_empty(myqueue* q) {
+static bool myqueue_is_empty(myqueue *q) {
     return STAILQ_EMPTY(q);
 }
 
-static void myqueue_push(myqueue* q, flower_t* flower_PTR) {
-    struct myqueue_entry* entry = malloc(sizeof(struct myqueue_entry));
+static void myqueue_push(myqueue *q, flower_t *flower_PTR) {
+    struct myqueue_entry *entry = malloc(sizeof(struct myqueue_entry));
     //entry->value = value;
     entry->flower_PTR = flower_PTR;
     STAILQ_INSERT_TAIL(q, entry, entries);
 }
 
-static flower_t* myqueue_pop(myqueue* q) {
+static flower_t *myqueue_pop(myqueue *q) {
     assert(!myqueue_is_empty(q));
-    struct myqueue_entry* entry = STAILQ_FIRST(q);
+    struct myqueue_entry *entry = STAILQ_FIRST(q);
     //const int value = entry->value;
-    flower_t* flower_PTR = entry->flower_PTR;
+    flower_t *flower_PTR = entry->flower_PTR;
     STAILQ_REMOVE_HEAD(q, entries);
     free(entry);
     return flower_PTR;
